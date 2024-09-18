b0c67b8097404c2151047c8374a99e2deb310b06
Vedant Gadge <vedant.gadgegsis@gmail.com>
2024-09-18 14:24:14 +0530

    18-9-24

--- q2210.java
+++ q2210.java
@@ -1,25 +1,23 @@
-//Incomplete
-
 public class q2210 {
-    public int countHillValley(int[] nums) {
-        int HnV = 0; //variable to track the count of hills n valleys
-        boolean HorV = true;
-        for(int i = 1;i<nums.length-1;i++){
-            if(nums[i]!= nums[i-1] && nums[i]!= nums[i+1] && HorV == false){
-                if((nums[i-1] >nums[i] && nums[i+1] >nums[i]) || (nums[i-1] <nums[i] && nums[i+1] <nums[i])){
-                    HnV++;
-                    HorV = true;
-                }
-                else HorV = true; 
-            }
-            else {
-                continue;
-            }
+    public static int countHillValley(int[] nums) {
+        int HnV = 0; // Variable to track the count of hills and valleys
+
+        for (int i = 1; i < nums.length - 1; i++) {
+           if(nums[i]==nums[i-1]) continue;
+
+           int prevE = i-1;
+           int nextE = i+1;
+           while(prevE>=0 && nums[prevE]==nums[i]) prevE --; //finds the closest unique elemnt on the left
+           while(nextE<nums.length && nums[nextE]==nums[i]) nextE ++; //finds the closest unique elemnt on the right
+            if(nextE>=nums.length || prevE<0) continue;
+           if((nums[i]>nums[prevE] && nums[i]>nums[nextE]) || (nums[i]<nums[prevE] && nums[i]<nums[nextE])) HnV ++;
         }
 
+        return HnV;
     }
 
     public static void main(String[] args) {
-        
+        int[] nums = {2, 4, 1, 1, 6, 5};
+        System.out.println(countHillValley(nums));
     }
 }
