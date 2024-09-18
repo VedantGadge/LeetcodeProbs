f59a295fbca4f061847f576b49e21d07f090f920
VedantGadge <vedant.gadgegsis@gmail.com>
2024-09-18 21:53:24 +0530

    .

---
+++ q3131.java
@@ -0,0 +1,17 @@
+public class q3131 {
+    public int addedInteger(int[] nums1, int[] nums2) {
+
+        int count = 0;
+        for (int i = 0; i < nums1.length; i++)
+            count += nums2[i] - nums1[i];
+
+        return count / nums1.length;
+    }
+
+    // This basically first calculates the diff of each element , to get the total of the diffrences
+    // then calculates the avg spread , by dividing the sum by nums.length().
+    public static void main(String[] args) {
+        int[] nums1 = {};
+        int[] nums2 = {};
+    }
+}
