5119090285b5c5fce18141279824d252ddbc2f04
Vedant Gadge <vedant.gadgegsis@gmail.com>
2025-05-29 22:02:39 +0530

    29-05-25

--- q189.java
+++ q189.java
@@ -1,24 +1,58 @@
-public class q189 {
+public class q189{
+
+    public static int[] reverse(int[] arr,int left,int right){
+        right--;
+        while(left<right){
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+        return arr;
+    }
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5,6,7};
-        int k =3;
+        int k = 3;
+        k = k % arr.length;
+        arr = reverse(arr, 0, arr.length);
+        arr = reverse(arr,0,k);
+        arr = reverse(arr, k, arr.length);
+        System.gc();
+         for(int i =0;i<arr.length;i++){
+             System.out.print(arr[i]);
+         }
 
-        for(int z =1;z<=k;z++){
-        int lastElement = arr[arr.length - 1];
-
-        // Shift all elements to the right
-        for (int i = arr.length - 1; i > 0; i--) {
-            arr[i] = arr[i - 1];
-            
-        }
-        // Set the first element to the previous last element
-        arr[0] = lastElement;
     }
-        for(int i =0;i<arr.length;i++){
-            System.out.print(arr[i]);
-        }
 }
-}
+
+
+
+
+
+//BRUTE FORCE
+
+// public class q189 {
+//     public static void main(String[] args) {
+//         int[] arr = {1,2,3,4,5,6,7};
+//         int k =3;
+
+//         for(int z =1;z<=k;z++){
+//         int lastElement = arr[arr.length - 1];
+
+//         // Shift all elements to the right
+//         for (int i = arr.length - 1; i > 0; i--) {
+//             arr[i] = arr[i - 1];
+            
+//         }
+//         // Set the first element to the previous last element
+//         arr[0] = lastElement;
+//     }
+//         for(int i =0;i<arr.length;i++){
+//             System.out.print(arr[i]);
+//         }
+// }
+// }
 
 /*
  i=6   1234566
@@ -28,3 +62,4 @@ public class q189 {
  i=2   1223456
  i=1   1123456
  */
+
