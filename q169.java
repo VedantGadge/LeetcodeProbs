46a4e4f59d6247d89be69a669805a273809ced63
Vedant Gadge <vedant.gadgegsis@gmail.com>
2025-05-31 22:22:14 +0530

    31-05-2025

---
+++ q169.java
@@ -0,0 +1,16 @@
+class q169{
+    public static void main(String[] args) {
+        int nums[] = {3,3,4,4,2,1,3};
+        int count = 0, majority=0;
+        for (int num:nums){
+            if(count==0){
+                majority = num;
+            } 
+            count += (num==majority)? 1 : -1;
+        }
+        System.out.println(majority);
+    }
+}
+
+//uses Bayer-Moore Majority Voting Algorithm
+//needs to have a majority candidate (appears more than n/2 times) 
