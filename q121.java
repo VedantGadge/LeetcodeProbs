23dd0a946a2da3ee1374e81fbd7c9c2478447653
Vedant Gadge <vedant.gadgegsis@gmail.com>
2025-06-01 23:58:59 +0530

    1-06-2025

---
+++ q121.java
@@ -0,0 +1,13 @@
+class q121{
+    public static void main(String[] args) {
+        int prices[] = {7,1,5,3,6,4};
+        int cost = 0, profit = 0;
+        int min = prices[0];
+        for (int i = 1;i<prices.length;i++){
+            cost = prices[i] - min;
+            profit = Integer.max(profit,cost);
+            min = Integer.min(min, prices[i]);
+        }
+        System.out.println(profit);
+    }
+}
