7046bd261bed95b7c776f62a31f41c45c42d8a97
Vedant Gadge <vedant.gadgegsis@gmail.com>
2025-06-04 01:00:55 +0530

    3-06-2025

---
+++ q54.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class q54 {
+    public static void main(String[] args) {
+        int[][] matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        int n = matrix.length;
+        int m = matrix[0].length;
+        List<Integer> ans = new ArrayList<>();
+        int left = 0, right = m - 1, top = 0, bottom = n - 1;
+        while (top <= bottom && left <= right) {
+
+            // left -> right
+            for (int i = left; i <= right; i++) {
+                ans.add(matrix[top][i]);
+            }
+            top++;
+
+            // top -> bottom
+            for (int i = top; i <= bottom; i++) {
+                ans.add(matrix[i][right]);
+            }
+            right--;
+
+            // right -> left
+            if (top <= bottom) { // checks if there is bottom row to go right to left
+                for (int i = right; i >= left; i--) {
+                    ans.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+
+            // bottom -> top
+            if (left <= right) { // checks if there is a left row to go bottom to top
+                for (int i = bottom; i >= top; i--) {
+                    ans.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+
+        // added if checks only for right -> left and bottom -> top as for earlier two ,
+        // the check are performed in their for and top gets ++ and right gets --;
+        for (int i : ans) {
+            System.out.print(i + " ");
+        }
+    }
+}
