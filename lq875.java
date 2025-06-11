622a4cee64c03b73f4d1c755ac66dc0ce7aefe55
VedantGadge <vedant.gadgegsis@gmail.com>
2025-06-11 23:24:46 +0530

    11-06-25

---
+++ lq875.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class lq875 {
+
+    static int maxElement(int[] arr){
+        int max = arr[0];
+        for (int num : arr) {
+            if (num > max) max = num;
+        }
+        return max;
+    }
+
+    static int calcTotHrs(int[] arr, int perhr){
+        int totalHr = 0;
+        for(int bananas: arr){
+            totalHr += Math.ceil((double)bananas/(double)perhr);
+        }
+        return totalHr;
+    }
+    public static void main(String[] args) {
+        int[] piles = {30,11,23,4,20};
+        int h = 6;
+        int low = 1 ,high = maxElement(piles);
+        while(low<=high){
+            int mid = low + (high - low)/2;
+            if(calcTotHrs(piles, mid) <= h){
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        System.out.println(low);
+    }
+}
