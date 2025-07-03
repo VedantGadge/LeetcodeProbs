85e4bc920224d06c06b052005696e96d32421f34
VedantGadge <vedant.gadgegsis@gmail.com>
2025-07-04 00:15:27 +0530

    04-07-25

---
+++ q3304.java
@@ -0,0 +1,17 @@
+public class q3304 {
+
+    static char kthCharacter(int k) {
+        StringBuilder sb = new StringBuilder("a");
+        while (sb.length() < k) {
+            int size = sb.length();
+            for (int i = 0; i < size; i++) {
+                sb.append((char) ('a' + ((sb.charAt(i) - 'a') + 1) % 26));
+            }
+        }
+        return sb.charAt(k - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(kthCharacter(5));
+    }
+}
