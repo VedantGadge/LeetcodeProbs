6c8f70f6485efe32ad319ed703b5dec74f6fb845
VedantGadge <vedant.gadgegsis@gmail.com>
2025-07-17 18:30:41 +0530

    17-07-25

---
+++ q503.java
@@ -0,0 +1,19 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class q503 {
+    public int[] nextGreaterElements(int[] nums) {
+        int[] nge = new int[nums.length];
+        Deque<Integer> stack = new ArrayDeque<>();
+        for(int i = 2*nums.length-1;i>=0;i--){
+            while(!stack.isEmpty() && stack.peek()<=nums[i%nums.length]){
+                stack.pop();
+            }
+            if(i<nums.length){
+                nge[i] = stack.isEmpty()? -1 : stack.peek();
+            }
+            stack.push(nums[i%nums.length]);
+        }
+        return nge;
+    }
+}
