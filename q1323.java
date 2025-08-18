3fc7bcef8249e0991fbdd54e097b2806c488249d
VedantGadge <vedant.gadgegsis@gmail.com>
2025-08-19 00:23:37 +0530

    19-08-25

--- q1323.java
+++ q1323.java
@@ -1,6 +1,13 @@
 public class q1323 {
     public int maximum69Number (int num) {
         int ans = 0, i = 0;
-        
+        while(num!=0){
+            int digit = num%10 ;
+            if(digit == 6) ans+= Math.pow(10, i)*9;
+            else ans+= Math.pow(10, i)*digit;
+            i++;
+            num /= 10;
+        }
+        return ans;
     }
 }
