47bdaf542de485b22907ce8699596ead6329cb0a
Vedant Gadge <vedant.gadgegsis@gmail.com>
2026-02-07 20:16:03 +0530

    check

---
+++ q3461.java
@@ -0,0 +1,19 @@
+class q3461{
+    public boolean hasSameDigits(String s) {
+         int n = s.length();
+        // convert to int array
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = s.charAt(i) - '0';
+        }
+        // reduce until size is 2
+        int size = n;
+        while (size > 2) {
+            for (int i = 0; i < size - 1; i++) {
+                a[i] = (a[i] + a[i + 1]) % 10;
+            }
+            size--;
+        }
+        return a[0] == a[1];
+    }
+}
