67dfd689492739acde6c57cd8bbf268227b55b1d
Vedant Gadge <vedant.gadgegsis@gmail.com>
2026-02-08 05:17:16 +0530

    harshil

--- q3461.java
+++ q3461.java
@@ -6,7 +6,6 @@ class q3461{
         for (int i = 0; i < n; i++) {
             a[i] = s.charAt(i) - '0';
         }
-        // reduce until 2 digit
         for (int size = n; size > 2; size--) {
             for (int i = 0; i < size - 1; i++) {
                 a[i] = (a[i] + a[i + 1]) % 10;
