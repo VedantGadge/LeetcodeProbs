9e5a2201a218ecd6ca0f1e67d737c08ff54fe2a7
Vedant Gadge <vedant.gadgegsis@gmail.com>
2026-02-08 07:56:14 +0530

    testt

--- q3461.java
+++ q3461.java
@@ -5,6 +5,7 @@ class q3461{
         for (int i = 0; i < n; i++) {
             a[i] = s.charAt(i) - '0';
         }
+        // sss
         for (int size = n; size > 2; size--) {
             for (int i = 0; i < size - 1; i++) {
                 a[i] = (a[i] + a[i + 1]) % 10;
